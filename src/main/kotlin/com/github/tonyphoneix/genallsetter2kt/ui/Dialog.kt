fe10ca7786bd1ac154468bbe14b1a9f260a9d351
mars <13093687239@163.com>
2020-09-02 17:16:44 +0800

    feat:优化样式

--- src/main/kotlin/com/github/tonyphoneix/genallsetter2kt/ui/Dialog.kt
+++ src/main/kotlin/com/github/tonyphoneix/genallsetter2kt/ui/Dialog.kt
@@ -3,9 +3,7 @@ package com.github.tonyphoneix.genallsetter2kt.ui
 import com.github.tonyphoneix.genallsetter2kt.entity.ParameterValue
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
-import wu.seal.jsontokotlin.ui.checkBox
-import wu.seal.jsontokotlin.ui.label
-import wu.seal.jsontokotlin.ui.verticalLinearLayout
+import wu.seal.jsontokotlin.ui.*
 import javax.swing.JComponent
 
 
@@ -21,20 +19,25 @@ class GenerateSetterFromParametersDialog(project: Project,
         super.init()
         setOKButtonText("Generate")
         title = "Generate all set methods from parameters";
+        if (parameters.size == 1) choices.add(parameters.first())
     }
 
     override fun createCenterPanel(): JComponent? {
         return verticalLinearLayout {
-            fixedSpace(5)
             label("<html>Check the parameters that need to be generated for <br>" +
-                    " the get method, multiple choices are available</html>")()
+                    " the get method, multiple choices are available</html>").putAlignLeft()
             val ps = this@GenerateSetterFromParametersDialog.parameters
             ps.forEach {
                 checkBox("${it.caller} : ${it.type.presentableText}", ps.size == 1) { isSelected ->
                     if (isSelected) choices.add(it) else choices.remove(it)
-                }()
+                }.putAlignLeft()
             }
-            if (ps.size == 1) choices.add(ps.first())
+            line()()
+            horizontalLinearLayout {
+                label("Like this plugin? Please star ")()
+                link("here", "https://github.com/TonyPhoneix/genallsetter2kt")()
+            }.putAlignLeft()
         }
+
     }
 }
